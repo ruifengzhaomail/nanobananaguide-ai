b038612b89f564494afff555c5e4c97f4f9841c0
User <user@example.com>
2025-09-12 10:17:11 +0800

    Add debug logs to Google Analytics component

--- app/google-analytics.tsx
+++ app/google-analytics.tsx
@@ -4,14 +4,20 @@ import Script from 'next/script'
 const GA_MEASUREMENT_ID = 'G-N43HLCBFK6'
 
 export default function GoogleAnalytics() {
+  // Debug log to ensure component is being rendered
+  console.log('GoogleAnalytics component rendering, NODE_ENV:', process.env.NODE_ENV)
+  
   return (
     <>
+      {/* Debug comment to verify component is rendered */}
+      {/* Google Analytics - GA_ID: G-N43HLCBFK6 */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
+          console.log('Google Analytics script executing');
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -19,6 +25,7 @@ export default function GoogleAnalytics() {
             page_title: document.title,
             page_location: window.location.href,
           });
+          console.log('Google Analytics configured with ID: ${GA_MEASUREMENT_ID}');
         `}
       </Script>
     </>
